eee158eb8b44828c0e1a702918494bdc6fc5438c
Uriel Nícolas Procópio Ramos <2022102020037@aluno.ifro.edu.br>
2024-08-27 09:54:09 -0400

    banco de dados inserido V2

--- RuralTech/Telas/Login.xaml.cs
+++ RuralTech/Telas/Login.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataBase.Configuracao;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -24,10 +25,49 @@ namespace RuralTech.Telas
             InitializeComponent();
         }
 
+        public bool ConsultarBancoDados(string usuario, string senha)
+        {
+           
+                bool validador = false;
+                var conexao = new Conexao();
+                var comando = conexao.Comando("SELECT * FROM usuario");
+                var leitor = comando.ExecuteReader();
+
+
+                while (leitor.Read())
+                {
+                    string nomeBanco = DAOHelper.GetString(leitor, "nome_usu");
+                    string senhaBanco = DAOHelper.GetString(leitor, "senha_usu");
+
+                    if (usuario.Equals(nomeBanco) && senhaBanco.Equals(senhaBanco))
+                    {
+                   
+                       
+                        validador = true;
+                    }
+                    
+
+            }
+            return validador;
+        }
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             var usuario = txt_usuario.Text;
             var senha = txt_senha.Password;
+
+            
+            if (ConsultarBancoDados(usuario, senha))
+            {
+                MessageBox.Show("Logado com sucesso!");
+                CadastroDeAnimal tela = new CadastroDeAnimal();
+                this.Close();
+                tela.ShowDialog();
+            }
+            else
+            {
+                MessageBox.Show("Usuario não cadastrado!");
+            }
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
